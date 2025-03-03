2b444e950b2499fcae82f7efaff3dda689fca9d4
greta <Djymini>
2025-03-03 16:49:50 +0100

    HomePage first version for validation

--- src/components/main/loginAndRegister/RegisterForm.tsx
+++ src/components/main/loginAndRegister/RegisterForm.tsx
@@ -1,17 +1,19 @@
 import {FC} from 'react';
 import '../../../@styles/b_main/components/loginAndRegister/form.css'
 
-const LoginForm: FC<{}> = ({}) => {
+const RegisterForm: FC<{}> = ({}) => {
     return (
         <form className={"subscribe-form"}>
-            <h2>Connection</h2>
+            <h2>Inscription</h2>
+            <label className={"form-label"}>Nom d'utilisateur</label>
+            <input name={"user-name"} type={"text"} id={"input-form"}/>
             <label className={"form-label"}>Email</label>
             <input name={"email"} type={"email"} id={"input-form"}/>
             <label className={"form-label"}>Mot de passe</label>
             <input name={"password"} type={"password"} id={"input-form"}/>
-            <button type={"submit"} className={"subscribe-button"}>Se connecter</button>
+            <button type={"submit"} className={"subscribe-button"}>S'inscrire</button>
         </form>
     );
 };
 
-export default LoginForm;
+export default RegisterForm;
--- src/pages/b_main/HomePage.tsx
+++ src/pages/b_main/HomePage.tsx
@@ -1,12 +1,14 @@
 import {FC} from 'react';
 import LoginForm from "../../components/main/loginAndRegister/LoginForm";
 import '../../@styles/b_main/pages/homePage.css';
+import RegisterForm from "../../components/main/loginAndRegister/RegisterForm";
 
 const HomePage: FC<{}> = ({}) => {
     return (
         <>
             <title>Page de d'accueil</title>
             <main>
+                <RegisterForm/>
                 <LoginForm/>
             </main>
         </>
