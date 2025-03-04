e17cbe0cc5d4ed1726f2a88ade9253a1b2635e28
greta <Djymini>
2025-03-04 10:39:14 +0100

    Update change the style of form

--- src/components/main/loginAndRegister/RegisterForm.tsx
+++ src/components/main/loginAndRegister/RegisterForm.tsx
@@ -11,6 +11,8 @@ const RegisterForm: FC<{}> = ({}) => {
             <input name={"email"} type={"email"} id={"input-form"}/>
             <label className={"form-label"}>Mot de passe</label>
             <input name={"password"} type={"password"} id={"input-form"}/>
+            <label className={"form-label"}>Confirmer le mot de passe</label>
+            <input name={"password-confirm"} type={"password"} id={"input-form"}/>
             <button type={"submit"} className={"subscribe-button"}>S'inscrire</button>
         </form>
     );
