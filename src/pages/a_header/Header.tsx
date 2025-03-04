3b9423e2175feae89e10a0942b25b12c9624293c
greta <Djymini>
2025-03-04 16:10:56 +0100

    AuthContext is created

--- src/pages/a_header/Header.tsx
+++ src/pages/a_header/Header.tsx
@@ -4,7 +4,7 @@ import '../../@styles/a_header/headerStyle.css';
 const Header: FC<{}> = ({}) => {
     return (
         <header>
-            <img src={"https://trello.com/1/cards/67b34f92a0acde729599d959/attachments/67bc720314b5fc34a296d213/download/Logo.png"}/>
+            <img src={"/LogoTheOffice.png"}/>
             <h1>The Office</h1>
         </header>
     );
