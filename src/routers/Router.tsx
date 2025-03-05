e9dccd04a7a74bc902f0ba2f6bef64759e4a0171
Johanna Keïta <veille.technologique.jk@gmail.com>
2025-03-05 15:57:19 +0100

    Added CreateCompany route in Rooter.tsx

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -3,6 +3,7 @@ import HomePage from "../pages/b_main/HomePage";
 import {Routes} from "react-router-dom";
 import {Route} from "react-router";
 import MainLayout from "../layout/MainLayout";
+import CreateCompanyPage from "../pages/b_main/CreateCompanyPage";
 
 const Router: FC<{}> = ({}) => {
     return (
@@ -10,6 +11,7 @@ const Router: FC<{}> = ({}) => {
             <Route path="/" element={<MainLayout/>}>
                 <Route path="/Home" element={<HomePage/>}/>
                 <Route path="/" element={<HomePage/>}/>
+                <Route path={"/create-company"} element={<CreateCompanyPage/>}/>
             </Route>
         </Routes>
     );
