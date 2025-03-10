a97271de8cc1f9afec468394d6d679fd2f09312d
greta <Djymini>
2025-03-10 20:50:09 +0100

    Locked company card is created

--- src/pages/b_main/CompanyPage.tsx
+++ src/pages/b_main/CompanyPage.tsx
@@ -7,10 +7,12 @@ import {getTheOfficeDbUser} from "../../api/theofficeApi";
 import {Company} from "../../_types/company";
 import CompanyCard from "../../components/main/companyPage/CompanyCard";
 import {createCompanyData, sectorCompanyData} from "../../_data/createCompanyData";
+import LockedCompanyCard from "../../components/main/companyPage/LockedCompanyCard";
 
 const CompanyPage: FC = () => {
     const [formIsVisible, setFormIsVisible] = useState<boolean>(false)
     const [arrayCompany, setArrayCompany] = useState<Company[]|any>([])
+    const limitCompany = 2
 
     useEffect(() => {
         collectUserCompanies()
@@ -36,7 +38,11 @@ const CompanyPage: FC = () => {
             <section className={"office-background"}>
                 {!formIsVisible ?
                     <>
-                        <CreateCompanyCard setFormIsVisible={setFormIsVisible}/>
+                        {arrayCompany.length >= limitCompany ?
+                            <LockedCompanyCard/>
+                            :
+                            <CreateCompanyCard setFormIsVisible={setFormIsVisible}/>
+                        }
                         {arrayCompany.length > 0 && arrayCompany.map((company:Company, index:number) => (
                             <CompanyCard key={index} companyName={company.name} pathImages={createCompanyData[sectorCompanyData.indexOf(company.sector)].src}/>
                         ))}
