27230427c9603ceaf147483cce2984f79f21f60d
greta <Djymini>
2025-03-10 11:04:52 +0100

    MainLayout is cleared

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -3,7 +3,7 @@ import Header from "../pages/a_header/Header";
 import {Outlet} from "react-router";
 import Footer from "../pages/c_footer/Footer";
 
-const MainLayout: FC<{}> = ({}) => {
+const MainLayout: FC = () => {
     return (
         <>
                 <Header userIsLogged={false} username={""}/>
