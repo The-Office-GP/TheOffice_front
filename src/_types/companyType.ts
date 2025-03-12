03976330a0ed9a8a8ee2d6d1877092ae26179a34
greta <Djymini>
2025-03-12 18:22:24 +0100

    Create the manage of Blob

--- src/_types/companyType.ts
+++ src/_types/companyType.ts
@@ -8,3 +8,28 @@ export interface CompanyType {
     employees: null;
     events: null;
 }
+
+export interface CompanyDetailsType {
+    sector: string;
+    name: string;
+    idUser: number;
+    wallet: number;
+    cycles: [];
+    machines: [];
+    employees: [];
+    suppliers: [];
+    events: [];
+    stockMaterials: [];
+    stockFinalMaterials: [];
+    local: localType;
+}
+
+export interface localType {
+    id: number;
+    level: string;
+    size: number;
+    rent: number;
+    maxEmployees: number;
+    maxMachines: number;
+    background_image: string;
+}
--- src/pages/b_main/CompanyPage.tsx
+++ src/pages/b_main/CompanyPage.tsx
@@ -1,17 +1,46 @@
-import {FC, useEffect} from 'react';
+import {Dispatch, FC, SetStateAction, useEffect, useState} from 'react';
 import GameMenu from "../../components/main/companyPage/GameMenu";
 import MiniDashboard from "../../components/main/companyPage/MiniDashboard";
 import "../../@styles/b_main/pages/companyPage.css"
 import {useParams} from "react-router";
+import {getTheOfficeDbUser} from "../../api/theofficeApi";
+import {getToken} from "../../utilis/storage";
 
 const CompanyPage: FC<{}> = ({}) => {
     const {id} = useParams()
+    const [url, setUrl] = useState<string>("")
 
     useEffect(() => {
-        console.log(id)
+        collectCompanyInfos()
     }, []);
+
+    useEffect(() => {
+        console.log(url)
+    }, [url]);
+
+    const collectCompanyInfos = async () => {
+        try {
+            const response = await getTheOfficeDbUser(`/companies/${id}`, getToken());
+            console.log(response);
+
+            const base64Image = response.local.background_image;
+
+            if (base64Image && base64Image.startsWith('iVBOR')) {
+                const byteArray = Uint8Array.from(atob(base64Image), c => c.charCodeAt(0));
+                const blob = new Blob([byteArray], {type: 'image/png'});
+                const imageUrl = URL.createObjectURL(blob);
+                setUrl(imageUrl);
+            } else {
+                console.error('Image en base64 invalide');
+            }
+        } catch (error) {
+            console.error('Erreur lors de la connexion:', error);
+        }
+    };
+
+
     return (
-        <section className={"background-company-model1-level1"}>
+        <section className={"background-company-model1-level1"} style={{backgroundImage: `url(${url})`}}>
             <div className={"nav-mini-dashbord"}>
                 <GameMenu/>
                 <MiniDashboard/>
