6ac3c5c839f3ff8a84a6a59dd9561a59e8df5d6b
Johanna Keïta <veille.technologique.jk@gmail.com>
2025-03-20 11:19:25 +0100

    Added simulation results board with components

--- src/pages/CompanyPage.tsx
+++ src/pages/CompanyPage.tsx
@@ -10,7 +10,11 @@ import EmployeeBoard from "../components/main/companyPage/boards/EmployeeBoard";
 import {CompanyContext} from "../contexts/CompanyContext";
 import {UserContext, UserContextProps} from "../contexts/UserContext";
 import MachineBoard from "../components/main/companyPage/boards/MachineBoard";
+<<<<<<< HEAD
 import {collectCompanyInfos} from "../@scripts/main/components/companyPage/companyPageScript";
+=======
+import SimulationBoard from "../components/main/companyPage/boards/SimulationBoard";
+>>>>>>> 1364c47 (Added simulation board with components)
 
 const CompanyPage: FC<{}> = ({}) => {
     const {id} = useParams()
@@ -32,6 +36,7 @@ const CompanyPage: FC<{}> = ({}) => {
 
 
     return (
+<<<<<<< HEAD
         <>
             <title>{company.name}</title>
             <CompanyContext.Provider value={{company, setCompany}}>
@@ -50,6 +55,24 @@ const CompanyPage: FC<{}> = ({}) => {
                 </section>
             </CompanyContext.Provider>
         </>
+=======
+        <CompanyContext.Provider value={{company, setCompany}}>
+            <section className={"background-company-model1-level1"}
+                     style={{backgroundImage: `url(${process.env.PUBLIC_URL}${url})`}}>
+                {statePage === 0 &&
+                    <div className={"nav-mini-dashbord"}>
+                        <GameMenu setPage={setStatePage}/>
+                        <MiniDashboard company={company} wallet={userContext.userInfo.wallet}/>
+                    </div>
+                }
+                {statePage === 1 && <GameDashboard setPage={setStatePage}/>}
+                {statePage === 2 && <MachineBoard setPage={setStatePage}/>}
+                {statePage === 3 && <EmployeeBoard setPage={setStatePage}/>}
+                {statePage === 4 && <SimulationBoard/>}
+                <h3 className={"level"}>{level}</h3>
+            </section>
+        </CompanyContext.Provider>
+>>>>>>> 1364c47 (Added simulation board with components)
     );
 };
 
