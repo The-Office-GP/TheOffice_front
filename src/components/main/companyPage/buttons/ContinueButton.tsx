2af99e0bde55cb3c19a65e6b66cd3ef312274c1c
Johanna Keïta <veille.technologique.jk@gmail.com>
2025-03-22 14:09:06 +0100

    Fixing simulation

--- src/components/main/companyPage/buttons/ContinueButton.tsx
+++ src/components/main/companyPage/buttons/ContinueButton.tsx
@@ -1,10 +1,8 @@
 import {Dispatch, FC, SetStateAction} from 'react';
 import "../../../../@styles/main/components/companyPage/simulation/ContinueButton.css"
 
-interface ContinueButtonProps {
-    setPage: (page: number) => void;
-}
-const ContinueButton: FC<ContinueButtonProps > = ({setPage}) => {
+
+const ContinueButton: FC<{ setPage: Dispatch<SetStateAction<number>> }> = ({setPage}) => {
     return (
         <>
             <button className={"continue-button"} onClick={() => setPage(0)}>Continuer</button>
--- src/components/main/companyPage/table/SimulationTable.tsx
+++ src/components/main/companyPage/table/SimulationTable.tsx
@@ -1,8 +1,9 @@
-import {FC} from 'react';
-import "../../styles/components/table/SimulationTable.css"
+import {FC, JSXElementConstructor, ReactElement, ReactNode, ReactPortal} from 'react';
+import "../../../../@styles/main/components/simulation/"
+
 
 const SimulationTable: FC<{}> = ({}) => {
-    const data = [
+    const data: any = [
         {produit: "Palette", bas: "0 unités", milieu: "0 unités", haut: "0 unités"},
         {produit: "Chaise", bas: "0 unités", milieu: "0 unités", haut: "0 unités"},
         {produit: "Table", bas: "0 unités", milieu: "0 unités", haut: "0 unités"},
@@ -20,9 +21,9 @@ const SimulationTable: FC<{}> = ({}) => {
                 </tr>
                 </thead>
                 <tbody>
-                {data.map((row, index) => (
+                {data.map((row: { produit: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; bas: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; milieu: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; haut: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; }, index:number) => (
                     <tr key={index} className="text-center">
-                        <td className="cellule">{row.produit}</td>
+                        Snapshot_2025-03-22_14-03-48.png          <td className="cellule">{row.produit}</td>
                         <td className="cellule">{row.bas}</td>
                         <td className="cellule">{row.milieu}</td>
                         <td className="cellule">{row.haut}</td>
