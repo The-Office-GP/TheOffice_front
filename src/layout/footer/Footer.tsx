3ab8a5fc4a066ecdbdd6b4cbe18ba37374fe29d3
Kevin8637 <kevin.lar@live.fr>
2025-03-22 14:19:58 +0100

    Add Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,10 +1,14 @@
 import {FC} from 'react';
-import '../../@styles/footer/footerStyle.css'
+import '../../@styles/main/components/global/Footer.css';
 
-const Footer: FC<{}> = ({}) => {
+const Footer: FC<{}> = () => {
     return (
-        <footer>
-
+        <footer className="footer">
+            <div className="footer-content">
+                <p>&copy; 2025 TheOfficeTheGame</p>
+                <img src={"../../../../../public/images/LogoTheOffice.png"} alt="logo">
+                <p>Tous droits réservés</p>
+            </div>
         </footer>
     );
 };
